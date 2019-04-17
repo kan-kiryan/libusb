b58a2806fc52a3bd4020086a485a08b177c33e03
Josh Gao <jmgao@google.com>
2019-04-17 16:01:52 -0700

    [FIX] some -Wformat warnings

--- libusb/os/linux_usbfs.c
+++ libusb/os/linux_usbfs.c
@@ -681,7 +681,7 @@ static int sysfs_get_active_config(struct libusb_device *dev, int *config)
 	close(fd);
 	if (r < 0) {
 		usbi_err(DEVICE_CTX(dev),
-			"read bConfigurationValue failed ret=%d errno=%d", r, errno);
+			"read bConfigurationValue failed ret=%zd errno=%d", r, errno);
 		return LIBUSB_ERROR_IO;
 	} else if (r == 0) {
 		usbi_dbg("device unconfigured");
@@ -1021,7 +1021,7 @@ static int initialize_device(struct libusb_device *dev, uint8_t busnum,
 		fd = wrapped_fd;
 		r = lseek(fd, 0, SEEK_SET);
 		if (r < 0) {
-			usbi_err(ctx, "seek failed ret=%d errno=%d", r, errno);
+			usbi_err(ctx, "seek failed ret=%zd errno=%d", r, errno);
 			return LIBUSB_ERROR_IO;
 		}
 	}
@@ -1235,7 +1235,7 @@ void linux_device_disconnected(uint8_t busnum, uint8_t devaddr)
 			usbi_disconnect_device (dev);
 			libusb_unref_device(dev);
 		} else {
-			usbi_dbg("device not found for session %x", session_id);
+			usbi_dbg("device not found for session %lx", session_id);
 		}
 	}
 	usbi_mutex_static_unlock(&active_contexts_lock);
@@ -1532,7 +1532,7 @@ uint8_t API_EXPORTED lib_usb_linux_get_dev_addr(libusb_device* dev) {
 
 static libusb_device* op_device2(struct libusb_context *ctx, const char *dev_node, int fd) {
 	uint8_t busnum, devaddr;
-	unsigned int session_id;
+	unsigned long session_id;
 	if (linux_get_device_address(ctx, 0, &busnum, &devaddr,
 		dev_node, NULL, fd) != LIBUSB_SUCCESS) {
 		usbi_dbg("failed to get device address (%s)", dev_node);
@@ -2498,7 +2498,7 @@ static int handle_bulk_completion(struct usbi_transfer *itransfer,
 			unsigned char *target = transfer->buffer + itransfer->transferred;
 			usbi_dbg("received %d bytes of surplus data", urb->actual_length);
 			if (urb->buffer != target) {
-				usbi_dbg("moving surplus data from offset %d to offset %d",
+				usbi_dbg("moving surplus data from offset %zd to offset %zd",
 					(unsigned char *) urb->buffer - transfer->buffer,
 					target - transfer->buffer);
 				memmove(target, urb->buffer, urb->actual_length);
